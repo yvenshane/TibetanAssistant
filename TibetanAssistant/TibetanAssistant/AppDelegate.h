57feecf6fe6ef26f0a738878fc935dd88469b511
YVEN SHANE <yvenshane@aol.com>
2018-09-13 19:19:38 +0800

    Create project

---
+++ TibetanAssistant/TibetanAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TibetanAssistant
+//
+//  Created by YVEN on 2018/9/13.
+//  Copyright © 2018年 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
